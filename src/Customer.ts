06ec110db54a3ad091c4e449fa071cba4cba5af1
fana3225 <acaca2234@gmail.com>
2025-11-26 21:06:48 -0300

    criacao de classes

---
+++ src/Customer.ts
@@ -0,0 +1,20 @@
+export default class Customer {
+    name: string ;
+    age: number;
+    email: string;
+    birthDate: Date;
+
+    constructor(name: string, age: number, email: string, birthDate: Date) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.age = new Date().getFullYear() - birthDate.getFullYear();
+    }
+
+    isAdult(): boolean {
+        return this.age >= 18;
+    }
+    getFirstName(): string {
+        return this.name.split(" ")[0];
+    }
+}
