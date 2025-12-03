6a6112436b44ca28fc7db72ae40a8bae09d774a4
fana3225 <acaca2234@gmail.com>
2025-12-03 20:09:23 -0300

    alt2

--- src/Customer.ts
+++ src/Customer.ts
@@ -1,15 +1,24 @@
 export default class Customer {
+    getlastName(): any {
+        throw new Error("Method not implemented.");
+    }
+    isAdult(): any {
+        throw new Error("Method not implemented.");
+    }
     name: string ;
     age: number;
     email: string;
     birthDate: Date;
     address?: string;
 
-    constructor(name: string, age: number, email: string, birthDate: Date) {
+    constructor(name: string, age: number, email: string, birthDate: Date, address?: string) {
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
         this.age = new Date().getFullYear() - birthDate.getFullYear();
+        this.address = address;
+        
+        }
     }
 
     isAdult(): boolean {
@@ -21,5 +30,9 @@ export default class Customer {
     getlastName(): string {
         return this.name.split( "")[this.name.split(" ").length -1];
     }
+}
+
+function isAdult() {
+    throw new Error("Function not implemented.");
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-
 import Customer from "./Customer"
+import Address from "./address";
+
+const address1 = new Address("Rua A", "Bairro B", "Cidade C", "Estado D", "12345-678");
+const address2 = new Address("Rua X", "Bairro Y", "Cidade Z", "Estado W", "87654-321");
 
 const Customer1 = new Customer("Ana", 33, "ana@email.com", new Date(1992, 11, 27));
 const Customer2 = new Customer("Ana Caroline Silva" ,35, "contato@ronaldo.com.br", new Date(2014,10,0));
@@ -7,4 +10,6 @@ const Customer2 = new Customer("Ana Caroline Silva" ,35, "contato@ronaldo.com.br
 console.log(Customer1.isAdult());
 console.log(Customer1.getlastName());   
 console.log(Customer2.isAdult());
-console.log(Customer2.getFirstName());
+console.log(Customer2.getlastName());       
+console.log(address1.tostring());
+console.log(address2.tostring());       
