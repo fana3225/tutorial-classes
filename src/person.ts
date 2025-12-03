82a756f2a950ec8fcdd0fe475fb37578d58f9658
fana3225 <acaca2234@gmail.com>
2025-12-03 20:24:15 -0300

    teste

--- src/person.ts
+++ src/person.ts
@@ -1,4 +1,18 @@
-export default class person {
-    
-
+export default class Person {
+name: string;
+age: number;
+email: string;
+birthDate: Date;
+constructor(name: string, email: string, birthDate: Date) {
+this.name = name;
+this.email = email;
+this.birthDate = birthDate;
+this.age = new Date().getFullYear() - birthDate.getFullYear();
+}
+isAdult() {
+return this.age >= 18;
+}
+getFirstName() {
+return this.name.split(" ")[0];
+}
 }
